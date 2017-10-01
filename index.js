93db41ffa5675833ef6380fc9aba23b82bc30955
Nastya Solovtsova <n-1999@mail.ru>
2017-10-01 15:01:06 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+$(function(){
+  $(".hover").hover(function(e) {
+    $(".title").toggleClass("active");
+  });
+});
+
+  
---
+++ sound-mouseover.js
@@ -0,0 +1,17 @@
+
+function playclip() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "Synth-pad-stab-G-minor-140-bpm.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByTagName("audio")[0];
+audio.play();
+}
+}
+}
+ 
